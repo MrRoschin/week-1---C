0b1b7d4bbb31385abd56ba0ef3d1ef1d407e4fea
MrRoschin <mrroschin@gmail.com>
2024-11-07 08:34:43 +1100

    added meow

---
+++ meow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 3; //initialise counter
+    while (i > 0) //loop to print now
+    {
+        printf("meow\n");
+        i--; //reduce counter by 1
+    }
+}
